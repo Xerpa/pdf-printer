76f2ca057a88f1c3498629a30ac33b7885c0253b
mynomoto <mynomoto@gmail.com>
2017-09-19 14:50:59 -0300

    Add a pdf printer using puppeteer

---
+++ server.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+const [ , , inputUrl, outputFile] = process.argv
+
+async function main () {
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  try {
+    await page.goto(inputUrl, {waitUntil: 'load'});
+  } catch(err) {
+    console.log('Error: ', err);
+    process.exit(1);
+  }
+  await page.pdf({path: outputFile, format: 'A4', landscape: true, printBackground: true});
+
+  await browser.close();
+}
+
+if (inputUrl && outputFile) {
+  main()
+} else {
+  console.log("inputUrl and outputFile are required")
+  process.exit(1);
+}

e26b2b500f03b0b013cd955f81c47a729cb4e0d1
mynomoto <mynomoto@gmail.com>
2017-09-26 14:12:22 -0300

    Only print the pdf on success

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ async function main () {
   const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   try {
-    await page.goto(inputUrl, {waitUntil: 'load'});
+    var response = await page.goto(inputUrl, {waitUntil: 'load'});
   } catch(err) {
     console.log('Error: ', err);
     process.exit(1);
   }
-  await page.pdf({path: outputFile, format: 'A4', landscape: true, printBackground: true});
+
+  if (response.ok) {
+    await page.pdf({path: outputFile, format: 'A4', landscape: true, printBackground: true});
+  } else {
+    console.log('Error: Invalid status(', response.status, ')');
+    process.exit(1);
+  }
 
   await browser.close();
 }
